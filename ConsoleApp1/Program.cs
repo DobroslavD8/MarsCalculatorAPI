d6c8b399bab8e6f0aa5738db301fe3db7173da6b
Dobtz <dobs7zz@outlook.com>
2018-10-15 17:38:15 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using MarsCalculatorAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            MarsCalculator mars = new MarsCalculator();
+
+            Console.Write("Enter first number: ");
+            string firstNumber = Console.ReadLine();
+
+            Console.Write("Enter second number: ");
+            string secondNumber = Console.ReadLine();
+
+            Console.Write("Enter operator: ");
+            string oper = Console.ReadLine();
+
+            if (oper == "+")
+            {
+                string plus = mars.Add(firstNumber, secondNumber);
+                Console.WriteLine("The result is: {0}.", plus);
+            }
+            else if (oper == "-")
+            {
+                string substr = mars.Substract(firstNumber, secondNumber);
+                Console.WriteLine("The result is: {0}.", substr);
+            }
+            else if (oper == "*")
+            {
+                string multi = mars.Multiply(firstNumber, secondNumber);
+                Console.WriteLine("The result is: {0}.", multi);
+            }
+            else if (oper == "/")
+            {
+                string dvd = mars.Divide(firstNumber, secondNumber);
+                Console.WriteLine("The result is: {0}.", dvd);
+            }
+            else {
+                Console.WriteLine("Enter valid operator!");
+            }
+
+           /* string addition = mars.Add("5", "5");
+            Console.WriteLine("Addition 5 and 5. Result: {0}. \n", addition);
+
+            string substract = mars.Substract("7", "5");
+            Console.WriteLine("Substract 7 and 5.Result: {0}. \n", substract);
+
+            string multiply = mars.Multiply("10", "5");
+            Console.WriteLine("Multiply 10 and 5. Result: {0}. \n", multiply);
+
+            string divide = mars.Divide("25", "5");
+            Console.WriteLine("Divide 25 and 5. Result: {0}. \n", divide);
+            */
+
+        }
+    }
+}
---
+++ MarsCalculatorAPI/MarsCalculator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsCalculatorAPI
+{
+    public class MarsCalculator
+    {
+        /// <summary>
+        /// Adding one number to another.
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        public string Add(string num1, string num2)
+        {
+            int octalNum1 = Convert.ToInt32(num1, 8);
+            int octalNum2 = Convert.ToInt32(num2, 8);
+            return Convert.ToString(octalNum1 + octalNum2, 8);
+        }
+
+        /// <summary>
+        /// Substracting one number from another.
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        public string Substract (string num1, string num2)
+        {
+            int octalNum1 = Convert.ToInt32(num1, 8);
+            int octalNum2 = Convert.ToInt32(num2, 8);
+            return Convert.ToString(octalNum1 - octalNum2, 8);
+        }
+
+        /// <summary>
+        /// Multiply two numbers.
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        public string Multiply (string num1, string num2)
+        {
+            int octalNum1 = Convert.ToInt32(num1, 8);
+            int octalNum2 = Convert.ToInt32(num2, 8);
+            return Convert.ToString(octalNum1 * octalNum2, 8);
+        }
+
+        /// <summary>
+        /// Dividing a number by another.
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        public string Divide (string num1, string num2)
+        {
+            int octalNum1 = Convert.ToInt32(num1, 8);
+            int octalNum2 = Convert.ToInt32(num2, 8);
+            return Convert.ToString(octalNum1 + octalNum2, 8);
+        }
+    }
+}
